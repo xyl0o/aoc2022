5224f4b2295e590314f1597b255a09767f8a7770
xyl0o <xyl0o@posteo.net>
2022-12-05 01:29:42 +0100

    [d1] gather cal of top three elves

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,11 @@ fn main() {
     let content = fs::read_to_string(file_path)
         .expect("Unable to read file");
 
+    most_cal(&content);
+    most_top3_cal(&content);
+}
+
+fn most_cal(content: &String) {
     let lines = content.lines();
 
     let mut most_cal: u32 = 0;
@@ -35,6 +40,30 @@ fn main() {
         let result: u32 = line.parse().expect("Couldn't parse line");
         curr_cal += result;
     }
+
     println!("Most cal: {most_cal}");
     println!("Elf with most cal: {most_cal_elf}");
+}
+
+
+fn most_top3_cal(content: &String) {
+    let lines = content.lines();
+
+    let mut cal_stack = [0, 0, 0, 0];
+
+    for line in lines {
+        if line == "" {
+            cal_stack.sort();
+            cal_stack[0] = 0;
+            continue;
+        }
+
+        let result: u32 = line.parse().expect("Couldn't parse line");
+        cal_stack[0] += result;
+    }
+
+    let top_three = &cal_stack[1..4];
+
+    println!("Top three cal: {:?}", top_three);
+    println!("Sum of top three cal: {:?}", top_three.iter().sum::<u32>());
 }
