71ae72068b393e95c88c60433a8fd9ee4563bff1
xyl0o <xyl0o@posteo.net>
2022-12-05 11:43:25 +0100

    [d3] add d3 skeleton

---
+++ src/day_three.rs
@@ -0,0 +1,19 @@
+
+pub fn day_three(input: String) {
+
+}
+
+fn test_foo() {
+    unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_foo() {
+        let result = 1;
+        assert_eq!(result, 1);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 
 mod day_one;
 mod day_two;
+mod day_three;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,6 +17,7 @@ fn main() {
     match day {
         1 => day_one::day_one(input),
         2 => day_two::day_two(input),
+        3 => day_three::day_three(input),
         _ => println!("Unknown day"),
     }
 }
