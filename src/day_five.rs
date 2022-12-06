9d6d6d9d125acbe581077792ed6764549270e77b
xyl0o <xyl0o@posteo.net>
2022-12-06 21:54:34 +0100

    [d5] add test for part one

--- src/day_five.rs
+++ src/day_five.rs
@@ -5,20 +5,28 @@ pub fn day_five(input: String) {
     todo!()
 }
 
-pub fn day_five_part_one(input: &str) -> u32 {
-    todo!()
-}
-
-pub fn day_five_part_two(input: &str) -> u32 {
+pub fn part_one(input: &str) -> String {
     todo!()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use indoc::indoc;
 
     #[test]
-    fn test_foo() {
-        todo!()
+    fn test_part_one() {
+        let puzzle_input = indoc! {"
+                [D]
+            [N] [C]
+            [Z] [M] [P]
+             1   2   3
+
+            move 1 from 2 to 1
+            move 3 from 1 to 3
+            move 2 from 2 to 1
+            move 1 from 1 to 2
+        "};
+        assert_eq!(part_one(puzzle_input), "CMZ");
     }
 }
