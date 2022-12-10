9c8288d36c4b147c85dca937d7693e28860c31bc
xyl0o <xyl0o@posteo.net>
2022-12-10 13:07:00 +0100

    [d6] implement part two

--- src/day_six.rs
+++ src/day_six.rs
@@ -20,7 +20,7 @@ pub fn part_one(input: &str) -> u32 {
 }
 
 pub fn part_two(input: &str) -> u32 {
-    todo!();
+    packet_start_pos_long(input, 14).unwrap()
 }
 
 fn packet_start_pos(stream: &str) -> Option<u32> {
@@ -29,18 +29,71 @@ fn packet_start_pos(stream: &str) -> Option<u32> {
         .tuple_windows::<(_, _, _, _)>()
         .map(|(a, b, c, d)| pairwise_distinct(&a, &b, &c, &d))
         .enumerate()
-        .skip_while(|(idx, unique)| !unique)
-        .map(|(idx, unique)| (idx + 4) as u32) // change when TryFrom is stable
+        .skip_while(|(_, unique)| !unique)
+        .map(|(idx, _)| (idx + 4) as u32) // change when TryFrom is stable
         .next()
+}
+
+fn packet_start_pos_long(stream: &str, window_size: usize) -> Option<u32> {
+    let mut stream_chars = stream.chars();
+    let mut window : Vec<char> = Vec::new();
+
+    while window.len() < (window_size - 1) {
+        window.push(match stream_chars.next() {
+            Some(c) => c,
+            None => return None,
+        });
+    }
+
+    for (idx, c) in stream_chars.enumerate() {
+        window.push(c);
+
+        if pairwise_distinct_long(&window) {
+            return Some((idx + window_size) as u32) // change when TryFrom is stable
+        }
+        // quite inefficient
+        window.remove(0);
+    }
+    None
 }
 
 fn pairwise_distinct(a: &char, b: &char, c: &char, d: &char) -> bool {
     !(a == b || a == c || a == d || b == c || b == d || c == d)
 }
 
+fn pairwise_distinct_long(v: &Vec<char>) -> bool {
+    // quite inefficient
+    let mut set: HashSet<char> = HashSet::new();
+    set.extend(v.iter());
+    set.len() == v.len()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_packet_start_window_too_long() {
+        let stream = "abc";
+        assert_eq!(packet_start_pos_long(stream, 4), None);
+        assert_eq!(packet_start_pos_long(stream, 20), None);
+    }
+
+    #[test]
+    fn test_packet_start_none_found() {
+        let stream = "abcabc";
+        assert_eq!(packet_start_pos_long(stream, 4), None);
+        let stream = "abababababababababababababababab";
+        assert_eq!(packet_start_pos_long(stream, 20), None);
+    }
+
+    #[test]
+    fn test_packet_start_found() {
+        let stream = "abcabc";
+        assert_eq!(packet_start_pos_long(stream, 3), Some(3));
+        let stream = "aaabcdefghijklmnopqrstuvwxyz";
+        assert_eq!(packet_start_pos_long(stream, 20), Some(22));
+    }
 
     #[test]
     fn test_distinct_all_same() {
@@ -59,5 +112,41 @@ mod tests {
     fn test_distinct_none_same() {
         assert_eq!(pairwise_distinct(&'a', &'b', &'c', &'d'), true);
         assert_eq!(pairwise_distinct(&'x', &'y', &'z', &'#'), true);
+    }
+
+    #[test]
+    fn test_distinct_long_all_same() {
+        assert_eq!(pairwise_distinct_long(&vec!['a', 'a', 'a', 'a']), false);
+        assert_eq!(pairwise_distinct_long(&vec!['0', '0', '0', '0']), false);
+    }
+
+    #[test]
+    fn test_distinct_long_some_same() {
+        assert_eq!(pairwise_distinct_long(&vec!['a', 'b', 'a', 'd']), false);
+        assert_eq!(pairwise_distinct_long(&vec!['x', 'A', '0', 'x']), false);
+        assert_eq!(pairwise_distinct_long(&vec!['a', 'a', 'a', 'x']), false);
+    }
+
+    #[test]
+    fn test_distinct_long_none_same() {
+        assert_eq!(pairwise_distinct_long(&vec!['a', 'b', 'c', 'd']), true);
+        assert_eq!(pairwise_distinct_long(&vec!['x', 'y', 'z', '#']), true);
+    }
+
+    #[test]
+    fn test_distinct_long_variable_length_true() {
+        assert_eq!(pairwise_distinct_long(&vec!['a']), true);
+        assert_eq!(pairwise_distinct_long(
+            &vec!['x', 'y', 'z', 'a', 'b', 'c']), true);
+    }
+
+    #[test]
+    fn test_distinct_long_variable_length_false() {
+        assert_eq!(pairwise_distinct_long(&vec!['a', 'a']), false);
+        assert_eq!(pairwise_distinct_long(&vec!['a', 'b']), true);
+        assert_eq!(pairwise_distinct_long(
+            &vec!['x', 'y', 'z', 'a', 'x', 'c']), false);
+        assert_eq!(pairwise_distinct_long(
+            &vec!['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a']), false);
     }
 }
--- tests/day_six.rs
+++ tests/day_six.rs
@@ -18,3 +18,21 @@ fn part_one_examples() {
     let puzzle_input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
     assert_eq!(part_one(puzzle_input), 11);
 }
+
+#[test]
+fn part_two_examples() {
+    let puzzle_input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    assert_eq!(part_two(puzzle_input), 19);
+
+    let puzzle_input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+    assert_eq!(part_two(puzzle_input), 23);
+
+    let puzzle_input = "nppdvjthqldpwncqszvftbrmjlhg";
+    assert_eq!(part_two(puzzle_input), 23);
+
+    let puzzle_input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    assert_eq!(part_two(puzzle_input), 29);
+
+    let puzzle_input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+    assert_eq!(part_two(puzzle_input), 26);
+}
