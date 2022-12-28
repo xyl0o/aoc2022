dce3342aa9f4498ee37f8e641f8d7f5d79b4f2f3
xyl0o <xyl0o@posteo.net>
2022-12-29 00:11:42 +0100

    [d11] add skeleton and tests for part one

---
+++ src/day_eleven.rs
@@ -0,0 +1,26 @@
+pub fn both(input: &str) {
+    let part_one_solution = part_one(input);
+    println!("Part one: {:?}", part_one_solution);
+
+    let part_two_solution = part_two(input);
+    println!("Part two: {:?}", part_two_solution);
+}
+
+pub fn part_one(input: &str) -> u32 {
+    todo!();
+}
+
+pub fn part_two(input: &str) -> String {
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn parse() {
+        todo!()
+    }
+}
